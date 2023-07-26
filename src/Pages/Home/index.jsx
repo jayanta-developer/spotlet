eae8dc5d3ba7cf7841434cac72e9968eb3d1ea30
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-07-26 22:31:23 +0530

    nav bar and gallery is done

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,97 @@
+import { Box, Typography, Divider } from '@mui/material'
+import React from 'react'
+
+import "./home.css"
+
+//Gallery Images
+import imageA from "../../Assets/images/galleryImages/Mask group.png"
+import imageB from "../../Assets/images/galleryImages/12.jpg"
+import imageC from "../../Assets/images/galleryImages/22.jpg"
+import imageD from "../../Assets/images/galleryImages/32.jpg"
+import imageE from "../../Assets/images/galleryImages/42.jpg"
+
+//white tab logo
+import print from "../../Assets/images/printerLogo.svg";
+import share from "../../Assets/images/share.svg";
+
+
+import locationIcon from "../../Assets/images/Group 54.svg"
+import starIcon from "../../Assets/images/star-48.png"
+
+export default function index() {
+  return (
+    <>
+      <Box className="homeContainer">
+
+        <Box className="topTabs">
+          <Box className="topTabBox">
+            <Box className="topWhiteTab">
+              <img className='topTabLogo' src={print} />
+              <Typography>Print</Typography>
+            </Box>
+
+            <Box sx={{ width: "90px" }} className="topWhiteTab">
+              <img className='topTabLogo' src={starIcon} />
+              <Typography>Favorite</Typography>
+
+            </Box>
+
+            <Box className="topWhiteTab">
+              <img className='topTabLogo' src={share} />
+              <Typography>Share</Typography>
+            </Box>
+          </Box>
+
+        </Box>
+
+        <Box className="coverGallery">
+          <Box className="firstGBox">
+            <img className='galleryImage' src={imageA} />
+          </Box>
+          <Box className="secondGBox">
+            <Box className="secondSBox">
+              <Box className="BgBox subImageBox"> <img className='galleryImage' src={imageE} /></Box>
+              <Box className="CgBox subImageBox"><img className='galleryImage' src={imageC} /></Box>
+            </Box>
+            <Box className="secondSBox">
+              <Box className="DgBox subImageBox"><img className='galleryImage' src={imageD} /></Box>
+              <Box className="EgBox subImageBox"><img className='galleryImage' src={imageB} /></Box>
+            </Box>
+          </Box>
+        </Box>
+
+
+
+        <Box className="venueLocation">
+          <Box mb={1.5} className="locationHeader">
+            <Typography sx={{ fontSize: "25px" }} className='poppinsBold' variant='h5'>Coworking Space: </Typography>
+            <Typography sx={{ marginLeft: "10px", fontSize: "25px" }} className='poppinsBold' variant='h5'>Serendipity Labs</Typography>
+          </Box>
+          <Box className="locationSubText">
+            <Box className="locationLogo"></Box>
+            <Box className="CityLocation">
+              <img src={locationIcon} style={{ marginRight: "15px" }} />
+              <Typography className='poppins'>Lonavala, Maharashtra, India</Typography>
+              <Divider sx={{ margin: "0px 10px" }} orientation="vertical" flexItem />
+              <Box sx={{ marginRight: "10px" }} className="startLogos">
+                <img src={starIcon} />
+                <img src={starIcon} />
+                <img src={starIcon} />
+                <img src={starIcon} />
+                <img src={starIcon} />
+              </Box>
+              <Typography className='poppins' sx={{ color: "#888888" }}>4.0 (3377 Reviews)</Typography>
+
+            </Box>
+            <Box className="venueRetting">
+
+            </Box>
+
+          </Box>
+
+        </Box>
+
+      </Box>
+    </>
+  )
+}
---
+++ src/Pages/NavBar/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Box, TextField, Typography } from '@mui/material'
+
+import "./nav.css";
+
+import companyLogo from "../../Assets/images/companyNavlogo.png";
+import SearchIcon from '../../Assets/images/search-normal.svg';
+import bail from "../../Assets/images/red bail.png"
+import star from "../../Assets/images/red star.png"
+import message from "../../Assets/images/red message.png";
+
+import Avatar from "../../Assets/images/Avatarprofile.png"
+import DownArrow from "../../Assets/images/downArrow.svg"
+
+
+export default function NavBar() {
+
+  return (
+    <>
+      <Box className="navBar">
+        <Box className="companyLogo">
+          <img src={companyLogo} />
+        </Box>
+
+        <Box className="searchBar">
+          <Box className="searchField">
+            <img style={{ width: "22px", position: "relative", left: "44px" }} src={SearchIcon} />
+            <TextField className='searchInput' sx={{ width: "100%" }} placeholder='   Enter Location' />
+          </Box>
+
+        </Box>
+
+
+
+        <Box className="notificationIcons">
+          <img src={bail} />
+          <img src={message} />
+          <img src={star} />
+        </Box>
+
+        <Box className="host">
+          <Typography className='poppins'>Become a Host</Typography>
+        </Box>
+        <Box className="booking">
+          <Typography className='poppins' sx={{ color: "#EA4335" }}>Bookings</Typography>
+        </Box>
+        <Box className="profile">
+          <Box className="profileTab">
+            <Box className="avatarBox">
+              <img style={{ width: "100%" }} src={Avatar} />
+            </Box>
+            <Typography ml={2} mr={1.5}>Krish Bhasin</Typography>
+            <img src={DownArrow} />
+
+          </Box>
+
+        </Box>
+      </Box>
+    </>
+  )
+}

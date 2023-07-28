a0a500f75170926893d0a0e4819b244f79203ce0
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-07-28 15:51:13 +0530

    fix properties card

--- src/Pages/SimilarProperties/index.jsx
+++ src/Pages/SimilarProperties/index.jsx
@@ -4,8 +4,8 @@ import "./properties.css"
 
 import Pimage1 from "../../Assets/images/properitiseImage/2021-10-23.jpg"
 import Pimage2 from "../../Assets/images/properitiseImage/Banquet-Hall-Gallery-15.jpeg"
-import Pimage4 from "../../Assets/images/properitiseImage/IMG-20221120-WA0148.jpg"
-import Pimage5 from "../../Assets/images/properitiseImage/WhatsApp Image 2023-07-15 at 12.29.22.jpeg"
+import Pimage3 from "../../Assets/images/properitiseImage/IMG-20221120-WA0148.jpg"
+import Pimage4 from "../../Assets/images/properitiseImage/WhatsApp Image 2023-07-15 at 12.29.22.jpeg"
 
 import buildingIcon from "../../Assets/images/properitiseImage/BildingIcon.png"
 import FilmIcon from "../../Assets/images/properitiseImage/FlimBoxIcon.png"
@@ -17,19 +17,19 @@ import GrayStar from "../../Assets/images/properitiseImage/GrayStar.png"
 
 export default function Properties() {
 
-  const propertiesCard = () => {
+  const propertiesCard = (img, fBoxHText, fBoxSText, locationHeader, address) => {
     return (
       <Box className="propertiesCard">
         <Box className="imageBox">
-          <img style={{ width: "100%" }} src={Pimage1} />
+          <img style={{ width: "100%", height: "100%" }} src={img} />
         </Box>
         <Box className="logo_Box">
           <img style={{ width: "25px", height: "25px" }} src={FilmIcon} />
         </Box>
         <Box className="CardTextBox">
           <Box className="cardText">
-            <Typography className='cardText1stHeader'>#SWISS23808</Typography>
-            <Typography className='cardSubText'>₹ 32000/8 hrs</Typography>
+            <Typography className='cardText1stHeader'>{fBoxHText}</Typography>
+            <Typography className='cardSubText'>{fBoxSText}</Typography>
           </Box>
 
           <Box className="cardText cardText2nd">
@@ -41,31 +41,10 @@ export default function Properties() {
               <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
               <span>(0)</span>
             </Box>
-            <Typography className='cardLocationHeader'>Cafe</Typography>
+            <Typography className='cardLocationHeader'>{locationHeader}</Typography>
             <Box className="cardLocationBox">
               <img style={{ width: "10px", height: "14px" }} src={LocationIcon} />
-              <span>Puppalguda, Sri Ram Nagar Colony</span>
-            </Box>
-          </Box>
-        </Box>
-        <Box className="CardTextBox">
-          <Box className="cardText">
-            <Typography className='cardText1stHeader'>#SWISS23808</Typography>
-            <Typography className='cardSubText'>₹ 32000/8 hrs</Typography>
-          </Box>
-          <Box className="cardText cardText2nd">
-            <Box className="cardStarBox">
-              <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
-              <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
-              <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
-              <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
-              <img style={{ width: "15px", height: "15px" }} src={GrayStar} />
-              <span>(0)</span>
-            </Box>
-            <Typography className='cardLocationHeader'>Cafe</Typography>
-            <Box className="cardLocationBox">
-              <img style={{ width: "10px", height: "14px" }} src={LocationIcon} />
-              <span>Puppalguda, Sri Ram Nagar Colony</span>
+              <span>{address}</span>
             </Box>
           </Box>
         </Box>
@@ -83,10 +62,34 @@ export default function Properties() {
         </Box>
         <Box className="propertiesBox">
           <Box className="propertiesRenderBox">
-            {propertiesCard()}
-            {propertiesCard()}
-            {propertiesCard()}
-            {propertiesCard()}
+            {propertiesCard(
+              Pimage1,
+              "#SWISS23808",
+              "₹ 32000/8 hrs",
+              "Cafe",
+              "Puppalguda, Sri Ram Nagar Colony"
+            )}
+            {propertiesCard(
+              Pimage2,
+              "#FUNLA36332",
+              "₹ 24000/8 hrs",
+              "Banquets Hall",
+              "Miyapur, Indira Nagar Colony"
+            )}
+            {propertiesCard(
+              Pimage3,
+              "#SUPRE95269",
+              "₹ 32000/8 hrs",
+              "GYM",
+              "Hyderabad, Sri Krishna Nagar"
+            )}
+            {propertiesCard(
+              Pimage4,
+              "#FILMN33108",
+              "₹ 24000/8 hrs",
+              "Studio Floors",
+              "Hyderabad, Film Nagar"
+            )}
 
           </Box>
         </Box>

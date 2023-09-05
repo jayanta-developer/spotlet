7bea869fff9b631e1710bff0d88eb966b1cf096b
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-09-05 13:09:02 +0530

    three pages routing done

---
+++ src/Pages/Blog/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./style.css";
+import { Box, TextField, Typography, Radio } from '@mui/material'
+
+//Components
+import NavBar from '../NavBar';
+import Footer from '../Footer';
+
+export default function Blog() {
+  return (
+    <>
+      <NavBar />
+      <Box className="blogContainer">
+
+      </Box>
+      <Footer />
+    </>
+  )
+}

f2dfa720510af36600a99a7c81839765306df49c
Lalit <loyjenaster@gmail.com>
2023-10-06 15:55:29 +0530

    change

--- src/Pages/LandingPageBanner/index.jsx
+++ src/Pages/LandingPageBanner/index.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./style.css";
-import { Box, Typography } from "@mui/material";
 
 //Owl Carousel Libraries and Module
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
-
-// import slide01 from "../../Assets/images/SliderImages/film-tv-shoot.jpg";
-// import slide02 from "../../Assets/images/SliderImages/corporate-retreat.jpg";
-// import slide03 from "../../Assets/images/SliderImages/personal-event.jpg";
 
 
 import slide01 from "../../Assets/images/SliderImages/film-tv-shoot2.jpeg";
